1fee595a7eab5e39d661e86f54799b305851da5d
Christophe Gibelli <cgibelli@tequilarapido.com>
2025-02-22 15:37:28 +0100

    Setup project with threejs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
+import Experience from './modules/experience';
 import './style.css'
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  new Experience({
+    domElement: document.getElementById('container')
+  });
+  
   console.log('Javascript is loaded !')
 })
---
+++ src/modules/Experience.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+export default class Experience {
+    constructor(options) {
+        this.container = options.domElement
+        this.width = window.innerWidth,
+        this.height = window.innerHeight 
+
+        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height , 0.01, 10 );
+        this.camera.position.z = 1;        
+
+        this.scene = new THREE.Scene();
+
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer.setSize( this.width, this.height );
+        this.container.appendChild(this.renderer.domElement)
+
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+        this.time = 0
+        this.resize();
+        this.addObjects();
+        this.render();
+        this.setupResize();
+    }
+
+    resize() {
+        // Update sizes
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+
+        // Update camera
+        this.camera.aspect = this.width / this.height;
+        this.camera.updateProjectionMatrix()
+
+        // Update renderer
+        this.renderer.setSize( this.width, this.height );
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    }
+
+    setupResize() {
+        window.addEventListener('resize', this.resize.bind(this))
+    }
+
+    addObjects() {
+        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.material = new THREE.MeshNormalMaterial();
+        
+        this.mesh = new THREE.Mesh( this.geometry, this.material );
+        this.scene.add( this.mesh );
+    }
+
+    render() {
+        this.mesh.rotation.x = this.time / 2000;
+        this.mesh.rotation.y = this.time / 1000;
+
+        this.renderer.render( this.scene, this.camera );
+
+        requestAnimationFrame(this.render.bind(this))
+    }
+}
