99ffa84da1fc6fb126966d2bdd5654f99a3d780c
Christophe Gibelli <cgibelli@tequilarapido.com>
2025-07-07 21:51:26 +0200

    Add a simple Waiting screen

---
+++ src/components/Wip.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wip">
+        <p class="entry">Website under construction</p>
+        <h1>Gibelli Christohe</h1>
+        <div class="sub-info">
+            <a class="linkedin" href="https://www.linkedin.com/in/christophe-gibelli/" aria-label="Gibelli Christophe LinkedIn profile">
+                <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M11.4559 11.4539H9.57998V8.51656C9.57998 7.81594 9.56725 6.91487 8.60509 6.91487C7.62766 6.91487 7.47749 7.67785 7.47749 8.46565V11.4539H5.60153V5.41372H7.4024V6.23907H7.42785C7.67857 5.76435 8.29074 5.26354 9.2039 5.26354C11.1047 5.26354 11.4559 6.5146 11.4559 8.14111V11.4539ZM3.46757 4.55054C2.8624 4.55054 2.37305 4.05992 2.37305 3.45602C2.37305 2.85213 2.86303 2.36214 3.46757 2.36214C4.07082 2.36214 4.56145 2.85276 4.56145 3.45602C4.56145 4.05992 4.07082 4.55054 3.46757 4.55054ZM2.54883 11.4538H4.42606V5.41355H2.54883V11.4538ZM12.727 0H1.2727C0.569532 0 0 0.569532 0 1.2727V12.727C0 13.4301 0.569532 13.9997 1.2727 13.9997H12.727C13.4308 13.9997 13.9997 13.4301 13.9997 12.727V1.2727C13.9997 0.569532 13.4308 0 12.727 0Z" fill="white"/>
+                </svg>
+            </a>
+            <span>/</span>
+            <p>Currently working at <a href="https://dusens.group/">Dusens Group</a></p>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
