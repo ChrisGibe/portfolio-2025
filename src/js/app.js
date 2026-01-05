cee4b6ae4841d0d975aa73c6f1b56a78a321549a
GIBELLI Christophe <cgibelli@tequilarapido.com>
2026-01-05 21:16:35 +0100

    Initialisation of the Threejs Starter

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+
+import Experience from './Experience/Experience'
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Hello World');
+
+    const experience = new Experience(document.querySelector('canvas.webgl'));
+});
